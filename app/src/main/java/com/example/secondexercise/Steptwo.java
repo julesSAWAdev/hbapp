fdc947e5a6615a8ec5570230b9ebc6e61cb5d849
Mugisha Jules Maurice <mugisha560@gmail.com>
2022-11-15 11:58:36 +0200

    added step two half way

--- app/src/main/java/com/example/secondexercise/Steptwo.java
+++ app/src/main/java/com/example/secondexercise/Steptwo.java
@@ -6,8 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Steptwo extends AppCompatActivity {
+    String[] response = new String[]{ "Yes","No","N/A"};
+    TextInputLayout organisationtxt,updatetxt;
+    AutoCompleteTextView organi,uptodate,accessible,labtecav,labtecsign,
+            labtecemp,nuresearav,nursearsign,nursearempsign,nursevacav,nursevacsign,nursevacempsign,custcareav,custcaresign,
+            custcareempsign,nursetbav,nursetbsign,nursetbempsign, nurchiav,nurchisign,nurchiempsign,socialav,socialsign,socialempsign,
+            nursecpnav,nursecpnsign,nursecpnempsign,midwifeav,midwifesign,midwifeempsign;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +28,113 @@ public class Steptwo extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        final String year = getIntent().getStringExtra("year_id");
-        final String district = getIntent().getStringExtra("district");
-        final String hc = getIntent().getStringExtra("hc");
+        //final String year = getIntent().getStringExtra("year_id");
+        //final String district = getIntent().getStringExtra("district");
+        //final String hc = getIntent().getStringExtra("hc");
 
-        Log.d("year", year);
-        Log.d("district", district);
-        Log.d("Hc", hc);
+        //Log.d("year", year);
+        //Log.d("district", district);
+        //Log.d("Hc", hc);
 
+       //organization chart
+        organi=findViewById(R.id.orgavtxt);
+        uptodate=findViewById(R.id.uptodatetxt);
+        accessible=findViewById(R.id.accessibletxt);
+        //lab technician
+        labtecav=findViewById(R.id.labtecavltxt);
+        labtecsign=findViewById(R.id.labtecsigntxt);
+       labtecemp=findViewById(R.id.labtecempsigntxt);
+       //NURSE AR
+       nuresearav=findViewById(R.id.nurseAravltxt);
+       nursearsign=findViewById(R.id.nurseArsigntxt);
+       nursearempsign=findViewById(R.id.nurseArempsigntxt);
+       //Nurse Vaccination
+        nursevacav=findViewById(R.id.nurseVacavltxt);
+        nursevacsign=findViewById(R.id.nurseVacsigned);
+        nursevacempsign=findViewById(R.id.nurseVacempsigntxt);
+        //files customer care
+        custcareav=findViewById(R.id.CustCareavltxt);
+        custcaresign=findViewById(R.id.CustCaresigntxt);
+        custcareempsign=findViewById(R.id.CustCareempsigntxt);
+
+        //files Nurse TB
+        nursetbav=findViewById(R.id.nurseTbavltxt);
+        nursetbsign=findViewById(R.id.nurseTbsigntxt);
+        nursetbempsign=findViewById(R.id.nurseTbempsigntxt);
+
+        //files nursechi
+        nurchiav=findViewById(R.id.nurseChiravltxt);
+        nurchisign=findViewById(R.id.nurseChirsigntxt);
+        nurchiempsign=findViewById(R.id.nurseChirempsigntxt);
+
+        //SOCIAL FILES
+        socialav=findViewById(R.id.socialavltxt);
+        socialsign=findViewById(R.id.socialsigntxt);
+        socialempsign=findViewById(R.id.socialempsigntxt);
+
+        //files nurse cpn
+        nursecpnav=findViewById(R.id.nurseCPNavltxt);
+        nursecpnsign=findViewById(R.id.nurseCPNsigntxt);
+        nursecpnempsign=findViewById(R.id.nurseCPNempsigntxt);
+
+        // midwife files hooks
+        midwifeav=findViewById(R.id.Midwifeavltxt);
+        midwifesign=findViewById(R.id.Midwifesigntxt);
+        midwifeempsign=findViewById(R.id.Midwifeempsigntxt);
+
+
+
+
+        ArrayAdapter<String> adapterDist = new ArrayAdapter<>(this, R.layout.dropdown_item2, response);
+
+        //organisation chart
+        organi.setAdapter(adapterDist);
+        uptodate.setAdapter(adapterDist);
+        accessible.setAdapter(adapterDist);
+        //lab technician
+        labtecav.setAdapter(adapterDist);
+        labtecsign.setAdapter(adapterDist);
+        labtecemp.setAdapter(adapterDist);
+        //nurse AR
+        nuresearav.setAdapter(adapterDist);
+        nursearsign.setAdapter(adapterDist);
+        nursearempsign.setAdapter(adapterDist);
+        //nurse vaccination
+        nursevacav.setAdapter(adapterDist);
+        nursevacsign.setAdapter(adapterDist);
+        nursevacempsign.setAdapter(adapterDist);
+        //customer care
+        custcareav.setAdapter(adapterDist);
+        custcaresign.setAdapter(adapterDist);
+        custcareempsign.setAdapter(adapterDist);
+        custcareempsign.setAdapter(adapterDist);
+
+        //nurse TB
+
+        nursetbav.setAdapter(adapterDist);
+        nursetbsign.setAdapter(adapterDist);
+        nursetbempsign.setAdapter(adapterDist);
+
+        // files petite churlgie
+        nurchiav.setAdapter(adapterDist);
+        nurchisign.setAdapter(adapterDist);
+        nurchiempsign.setAdapter(adapterDist);
+
+
+        //social file adapter
+        socialav.setAdapter(adapterDist);
+        socialsign.setAdapter(adapterDist);
+        socialempsign.setAdapter(adapterDist);
+
+        //Nurse cpn adapter
+        nursecpnav.setAdapter(adapterDist);
+        nursecpnsign.setAdapter(adapterDist);
+        nursecpnempsign.setAdapter(adapterDist);
+
+        //midwife adapter
+        midwifeav.setAdapter(adapterDist);
+        midwifesign.setAdapter(adapterDist);
+        midwifeempsign.setAdapter(adapterDist);
 
 
 
